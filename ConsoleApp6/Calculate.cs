930b41d7fecb4ebfc286d898a6342674eec81bf8
ekelemene <ksuxaskrip2007@gmail.com>
2025-02-14 12:14:51 +0200

    Add files via upload

---
+++ ConsoleApp6/Calculate.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    /// <summary>
+    /// класс описывающий вычисления
+    /// </summary>
+    class Calculate
+    {
+        /// <summary>
+        /// поля с переменными 
+        /// </summary>
+        private double num1;
+        private double num2;
+
+        public double Num1 { get => num1; set  { if (value < 0) throw new ArgumentException("error"); num1 = value; } }
+        public double Num2 { get => num2; set  { if (value < 0) throw new ArgumentException("error"); num2 = value; } }
+
+        
+
+        /// <summary>
+        /// конструктор без парам
+        /// </summary>
+        public Calculate()
+        {
+            Num1 = 0;
+            Num2 = 0;
+        }
+        /// <summary>
+        /// конструктор с 1 параметром
+        /// </summary>
+        /// <param name="Num1"></param>
+        public Calculate(double num_1)
+        {
+            this.Num1 = num_1;
+            this.Num2 = 0;
+        }
+        /// <summary>
+        /// конструктор с 2 параметрами 
+        /// </summary>
+        /// <param name="Num1"></param>
+        /// <param name="Num2"></param>
+        public Calculate(double num_1,double num_2)
+        {
+            this.Num1 = num_1;
+            this.Num2 = num_2;
+        }
+        /// <summary>
+        /// функция вывода строки
+        /// </summary>
+        /// <returns></returns>
+        public string GetInfo()
+        {
+            return $"num1 = {Num1} \nnum2 = {Num2}";
+        }
+        /// <summary>
+        /// метод для вычисления кв корня
+        /// </summary>
+        /// <returns></returns>
+        public double CalcNum()
+        {
+            return Math.Sqrt(Num1 * Num2);
+        }
+
+
+
+
+    }
+}
---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //пример без параметров
+            Calculate ob1 = new Calculate();
+            Console.WriteLine(ob1.GetInfo());
+            ob1.Num1 = 4;
+            ob1.Num2 = 9;
+            Console.WriteLine(ob1.GetInfo());
+            Console.WriteLine("result = " + ob1.CalcNum());
+            Console.WriteLine();
+            //пример с явной инициализацией
+            Calculate ob2 = new Calculate(1);
+            Console.WriteLine(ob2.GetInfo());
+            Calculate ob4 = new Calculate(7,3);
+            Console.WriteLine(ob4.GetInfo());
+            Console.WriteLine("result = " + ob4.CalcNum());
+            Console.WriteLine();
+            //пример введенный с клавиатуры
+            Console.Write("num1 = ");
+            int num1 = int.Parse(Console.ReadLine());
+            Console.Write("num2 = ");
+            int num2 = int.Parse(Console.ReadLine());
+            Calculate ob3 = new Calculate(num1,num2);
+            Console.WriteLine(ob3.GetInfo());
+            Console.WriteLine("result = "+ob3.CalcNum());
+
+            Console.Read();
+
+
+        }
+    }
+}
